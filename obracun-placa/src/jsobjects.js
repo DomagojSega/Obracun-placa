43b20c37fe6bbdd8c9275a1e97729ad363882191
goc10 <sfxgoc@gmail.com>
2023-05-21 22:42:33 +0200

    Cleaner code, added sessionStorage, handleLogout

---
+++ obracun-placa/src/jsobjects.js
@@ -0,0 +1,17 @@
+users = [
+    {
+    "id":1,
+    "username":"goc",
+    "password":"123",
+    "firstname":"Gordan",
+    "lastname":"Paun",
+    "role":"admin"
+    },
+    {
+    "id":2,
+    "username":"domi",
+    "password":"123",
+    "firstname":"Domagoj",
+    "lastname":"Šega",
+    "role":"user"
+}];
